bb8cbb9ecd44aecb7b83191c228c1bb867ded94a
HarshPatelWeb <harshpatel1242@gmail.com>
2024-05-09 21:48:05 -0400

    Data ingestion stage completed in pipeline.

---
+++ main.py
@@ -0,0 +1,14 @@
+from cnnClassifier import logger
+from src.cnnClassifier.config.configuration import ConfigurationManager
+from src.cnnClassifier.pipeline.stage_data_ingestion import DataIngestionTrainingPipeline
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
