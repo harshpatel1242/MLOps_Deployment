b555d1657afae56e58bfee5ba93960ea3bb862df
HarshPatelWeb <harshpatel1242@gmail.com>
2024-05-10 13:10:00 -0400

    Minor Changes

--- src/Classifer_Package/config/configuration.py
+++ src/Classifer_Package/config/configuration.py
@@ -75,7 +75,7 @@ class ConfigurationManager:
         eval_config = EvaluationConfig(
             path_of_model="artifacts/training/model.h5",
             training_data="artifacts/data_ingestion/kidney-ct-scan-image",
-            mlflow_uri="https://dagshub.com/entbappy/Kidney-Disease-Classification-MLflow-DVC.mlflow",
+            mlflow_uri="https://dagshub.com/harshpatel1242/MLOps_Deployment.mlflow",
             all_params=self.params,
             params_image_size=self.params.IMAGE_SIZE,
             params_batch_size=self.params.BATCH_SIZE
