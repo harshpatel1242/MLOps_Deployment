0f3c6d3be3faedf0b67134e3a8f92b2e85961ba2
HarshPatelWeb <harshpatel1242@gmail.com>
2024-05-09 23:49:09 -0400

    Model training stage added into pipeline

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from cnnClassifier import logger
 from src.cnnClassifier.config.configuration import ConfigurationManager
 from src.cnnClassifier.pipeline.stage_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_prepare_base_model import PrepareBaseModelTrainingPipeline
+from cnnClassifier.pipeline.stage_model_training import ModelTrainingPipeline
 
 
 
@@ -21,6 +22,17 @@ try:
    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
    prepare_base_model = PrepareBaseModelTrainingPipeline()
    prepare_base_model.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
         logger.exception(e)
